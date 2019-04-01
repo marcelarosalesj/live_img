22679718d779ec6285c01fb17d1a31168c151554
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-01 17:15:33 -0600

    Add first custom_iso.sh script
    It only generates the rootfs for now.
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

---
+++ custom_iso.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# I'm following this guide:
+# https://www.hiroom2.com/2016/06/10/ubuntu-16-04-create-customized-livedvd
+#
+
+set -x
+
+install_package() {
+	pkg=$1
+	dpkg -s $pkg &> /dev/null
+	retcode=${PIPESTATUS[0]}
+	if [ $retcode -eq 0 ]; then
+		echo "$pkg already installed"
+	else
+		echo "Installing $pkg..."
+		sudo apt-get install -y $pkg $> /dev/null
+		retcode=${PIPESTATUS[0]}
+		if [ $retcode -eq 1 ]; then
+			echo "Errors intalling $pkg"
+		fi
+	fi
+}
+
+# Install dependencies
+install_package livecd-rootfs
+install_package systemd-container
+install_package xorriso
+
+# Create rootfs
+echo "Create rootfs"
+mkdir live-build
+cd live-build/
+livebuilddir=/usr/share/livecd-rootfs/live-build
+if [ -d $livebuilddir ]; then
+	echo "Copying example files"
+	cp -a $livebuilddir/auto .
+	cp -a $livebuilddir/ubuntu-core .
+else
+	echo "Directory doesn't exists $livebuilddir"
+	echo "Check live-build installation"
+	exit -1
+fi
+
+# Generate ubuntu xenial config
+echo "Generate ubuntu config"
+export PROJECT="ubuntu-core"
+env | grep ubuntu
+
+if [ $# -eq 1 ]; then
+	http_proxy=$1
+	PROJECT="ubuntu-core" lb config --apt-http-proxy "$http_proxy" --apt-ftp-proxy "$http_proxy"
+else
+	PROJECT="ubuntu-core" lb config	
+fi
+
+sed -i 's/precise/xenial/g' config/bootstrap
+
+echo "Execute lb build"
+mkdir chroot
+sudo PROJECT="ubuntu-core" lb build
