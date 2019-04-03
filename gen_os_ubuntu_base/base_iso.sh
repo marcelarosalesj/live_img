c1ccc05c0dd327f3cf1c27647ccae4ec000c0758
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-03 17:12:54 -0600

    Clean-up ubuntu iso gen scripts
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- gen_os_ubuntu_base/base_iso.sh
+++ gen_os_ubuntu_base/base_iso.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+OS="ubuntu-base-16.04.6-base-amd64.tar.gz"
+URL="http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/$OS"
+CHROOT="chroot"
+
 install_package() {
 	pkg=$1
 	dpkg -s $pkg &> /dev/null
@@ -10,8 +14,6 @@ install_package() {
 		echo "$pkg already installed"
 	else
 		echo "Installing $pkg..."
-		#sudo apt-get install -y $pkg $> /dev/null
-		#retcode=${PIPESTATUS[0]}
 		sudo apt-get install -y $pkg
 		retcode=$?
 		if [ $retcode -eq 1 ]; then
@@ -28,50 +30,39 @@ install_package livecd-rootfs
 install_package systemd-container
 install_package xorriso
 
-if [ ! -e ubuntu-base-16.04.6-base-amd64.tar.gz ]; then
-    echo "ubuntu-base tarball already exists"
-    wget http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/ubuntu-base-16.04.6-base-amd64.tar.gz
-fi
+# Download OS tarball if it does not exist
+[ ! -e $OS ] && wget $URL
 
-if [ -d chroot ]; then
-    echo "deleting previous iso generation..."
-    sudo rm -rf chroot
-fi
+# Uncompress tarball in new CHROOT directory
+[ -d $CHROOT ] && sudo rm -rf $CHROOT
+mkdir $CHROOT
+tar -xvzf $OS -C $CHROOT
 
-mkdir chroot
-tar -xvzf ubuntu-base-16.04.6-base-amd64.tar.gz -C chroot
-
-echo ">> Adding config files"
-echo "ubuntu-live" | sudo tee chroot/etc/hostname
-echo "127.0.0.1 ubuntu-live" | sudo tee chroot/etc/hosts
-if [ ! -d chroot/boot/grub ]; then
-    echo "chroot/boot/grub dir doesn't exist, let's create it"
-    mkdir -p chroot/boot/grub
-fi
-cp grub.cfg chroot/boot/grub/
-cp config_rootfs.sh chroot/
+# Add some configuration
+echo "ubuntu-live" | sudo tee $CHROOT/etc/hostname
+echo "127.0.0.1 ubuntu-live" | sudo tee $CHROOT/etc/hosts
+[ ! -d "$CHROOT/boot/grub" ] && mkdir -p "$CHROOT/boot/grub"
+cp grub.cfg $CHROOT/boot/grub/
+cp config_rootfs.sh $CHROOT/
 
 # Allow empty password for root user
-cp chroot/etc/shadow chroot/etc/shadow_backup
-sed '/^root:/ s|\*||' -i chroot/etc/shadow
-cp chroot/etc/securetty chroot/etc/securetty_backup
+cp $CHROOT/etc/shadow $CHROOT/etc/shadow_backup
+sed '/^root:/ s|\*||' -i $CHROOT/etc/shadow
+cp $CHROOT/etc/securetty $CHROOT/etc/securetty_backup
 
-# Add special deb file to rootfb
-mkdir -p chroot/stxdebs
-cp fm-common-dev_0.0-1_amd64.deb chroot/stxdebs
-cp fm-mgr_0.0-1_amd64.deb chroot/stxdebs
+# Add StarlingX deb files to rootfs
+cp -r stxdebs $CHROOT/
+cp -r stxdebs $CHROOT/usr/local
 
 # Configure rootfs with config_rootfs.sh script
-sudo systemd-nspawn -D chroot --machine genubuntu ./config_rootfs.sh
+sudo systemd-nspawn -D $CHROOT --machine genubuntu ./config_rootfs.sh
 retcode=$?
 if [ $retcode -eq 0 ]; then
-    echo "continue with iso generation..."
-    echo ">> generate iso"
     mkdir -p iso/live
-    sudo cp -a chroot/boot/ iso/
-    sudo mksquashfs chroot iso/live/filesystem.squashfs
+    sudo cp -a $CHROOT/boot/ iso/
+    sudo mksquashfs $CHROOT iso/live/filesystem.squashfs
     sudo grub-mkrescue -o ubuntu-live.iso iso
 else
-    echo "error with systemd-nspawn..."
+    echo "Error with systemd-nspawn"
     exit -1
 fi
--- gen_os_ubuntu_base/config_rootfs.sh
+++ gen_os_ubuntu_base/config_rootfs.sh
@@ -2,14 +2,21 @@
 
 set -x
 
+update_debs(){
+    pushd /usr/local/stxdebs
+    dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+    popd
+}
+
 apt-get clean -y
 apt-get update -y
 apt-get upgrade -y
 
-apt-get install -y live-boot live-boot-initramfs-tools
-apt-get install -y grub2-common
-apt-get install -y grub
-apt-get install -y linux-image-4.4.0-143-generic
+apt-get install -y \
+    live-boot \
+    live-boot-initramfs-tools \
+    grub2-common \
+    linux-image-4.4.0-143-generic
 
 apt-get install -y \
 	network-manager \
@@ -20,32 +27,17 @@ apt-get install -y \
 	less \
 	lvm2 \
 	e2fsprogs \
-	net-tools
+	net-tools \
+    vim \
+    strace
 
-### To avoid dependency hell... ###
-echo "marj>> avoiding dependency hell..."
+# Create Apt Local Repo for StarlingX packages
 apt-get install -y dpkg-dev
-mkdir -p /usr/local/stxdebs
-cp -r /stxdebs/*.deb /usr/local/stxdebs/
-# Scan local repository and generate Packages.gz
-update_debs(){
-    pushd /usr/local/stxdebs
-    dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
-    popd
-}
-# Add local repo to sources.list
 sed -i '1s/^/deb [trusted=yes] file:\/usr\/local\/stxdebs .\/\n/' /etc/apt/sources.list
 update_debs
 apt-get update -y
 
-
-echo "marj>> installing fm-common and fm-mgr"
-ls /usr/local/stxdebs
-ls /stxdebs
+# Install StarlingX package
 pushd /stxdebs
 apt install -y ./fm-mgr_0.0-1_amd64.deb --allow-unauthenticated
 popd
-
-useradd -m -s /bin/bash marce
-gpasswd -a marce sudo
-
