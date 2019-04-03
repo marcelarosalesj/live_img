7f732a996aae95ff5cff03793bc68b788e667cc7
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-03 14:53:48 -0600

    Add to ubuntu systemd-nspawn local repository for apt
    - now when apt install happens, it looks for dependencies in the
    local repository too.
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- gen_os_ubuntu_base/config_rootfs.sh
+++ gen_os_ubuntu_base/config_rootfs.sh
@@ -22,11 +22,28 @@ apt-get install -y \
 	e2fsprogs \
 	net-tools
 
+### To avoid dependency hell... ###
+echo "marj>> avoiding dependency hell..."
+apt-get install -y dpkg-dev
+mkdir -p /usr/local/stxdebs
+cp -r /stxdebs/*.deb /usr/local/stxdebs/
+# Scan local repository and generate Packages.gz
+update_debs(){
+    pushd /usr/local/stxdebs
+    dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+    popd
+}
+# Add local repo to sources.list
+sed -i '1s/^/deb [trusted=yes] file:\/usr\/local\/stxdebs .\/\n/' /etc/apt/sources.list
+update_debs
+apt-get update -y
+
+
 echo "marj>> installing fm-common and fm-mgr"
+ls /usr/local/stxdebs
 ls /stxdebs
 pushd /stxdebs
-apt install -y ./fm-common-dev_0.0-1_amd64.deb
-apt install -y ./fm-mgr_0.0-1_amd64.deb
+apt install -y ./fm-mgr_0.0-1_amd64.deb --allow-unauthenticated
 popd
 
 useradd -m -s /bin/bash marce
